a3f4c2d20aeb41db8dc51b1e3d0d3b4db0d81562
Samuel Aguiar <samuelaguiarp@gmail.com>
2019-12-18 19:17:28 -0300

    gsap register plugin

--- src/timelines/index.js
+++ src/timelines/index.js
@@ -1,10 +1,12 @@
-import { Power1, TimelineMax } from "gsap/all";
+import { Power1, TimelineMax as Timeline, gsap } from "gsap";
 
 import { getFoward } from "../selector";
 import store from "../store";
 
+gsap.registerPlugin(Power1, Timeline);
+
 const getTimeline = (node, home, results, foward) => {
-  const timeline = new TimelineMax({ paused: true });
+  const timeline = new Timeline({ paused: true });
   if (node === null) {
     return timeline;
   }
@@ -13,7 +15,7 @@ const getTimeline = (node, home, results, foward) => {
     : node.querySelector(".container-inside");
 
   timeline.from(containerInside, 0.25, {
-    opacity: 0,
+    autoAlpha: 0,
     ease: Power1.easeIn,
     x: results ? 0 : foward ? 25 : -25
   });
@@ -22,8 +24,6 @@ const getTimeline = (node, home, results, foward) => {
 };
 
 export const play = (pathname, node) => {
-  console.log("i got here");
-
   const home = pathname === "/" ? true : false,
     results = pathname === "/Picks" || pathname === "/Purchase" ? true : false,
     foward = getFoward(store.getState()),
@@ -33,16 +33,16 @@ export const play = (pathname, node) => {
 };
 
 export const playError = (node, enter) => {
-  const timeline = new TimelineMax({ paused: true });
+  const timeline = new Timeline({ paused: true });
 
   enter
     ? timeline.from(node, 0.1, {
-        opacity: 0,
+        autoAlpha: 0,
         ease: Power1.easeIn,
         y: -5
       })
     : timeline.to(node, 0.1, {
-        opacity: 0,
+        autoAlpha: 0,
         ease: Power1.easeIn,
         y: -5
       });
@@ -51,10 +51,10 @@ export const playError = (node, enter) => {
 };
 
 export const playVertical = node => {
-  const timeline = new TimelineMax({ paused: true });
+  const timeline = new Timeline({ paused: true });
 
   timeline.from(node, 0.25, {
-    opacity: 0,
+    autoAlpha: 0,
     ease: Power1.easeIn,
     y: 5
   });
